950734ecf598d9cd32bbbcfc17c415d19758a5f4
konwrz20 <konwrz20@gmail.com>
2018-07-19 23:31:29 +0200

    prepare raw data

--- signals/MNE/ERP_MNE.py
+++ signals/MNE/ERP_MNE.py
@@ -1,7 +1,6 @@
 import mne
 from EegData import *
 
-
 file_path = 'signals/record-FGT-Z-[2017.12.20-08.38.23].csv'
 eeg_data = EegData(file_path)
 
@@ -12,9 +11,18 @@ electrodes_to_analyze = ['P7', 'O1', 'O2', 'P8']
 info = mne.create_info(
     ch_names=electrodes_to_analyze,
     ch_types=['eeg', 'eeg', 'eeg', 'eeg'],
-    sfreq=128
+    sfreq=128,
+    montage='standard_1020'
 )
 
-custom_raw = mne.io.RawArray(eeg_data.get_eeg_data(electrodes_to_analyze), info)
+raw = mne.io.RawArray(eeg_data.get_eeg_data(electrodes_to_analyze), info)
+raw.pick_types(meg=False, eeg=True, eog=False)
+print(raw.info)
 
-custom_epochs = mne.EpochsArray()
+raw.notch_filter([50, 60])
+raw.filter(1., 15, n_jobs=1, fir_design='firwin')
+
+raw.set_eeg_reference('average', projection=True)
+raw.plot(n_channels=raw.info['nchan'], block=True, scalings='auto', duration=500)
+
+
--- signals/MNE/EegData.py
+++ signals/MNE/EegData.py
@@ -2,15 +2,13 @@ import numpy as np
 
 
 class EegData:
-    _eeg_data = None
-
     def __init__(self, file_path):
-        self.read_eeg_data_from_csv(file_path)
+        self._eeg_data = self.read_eeg_data_from_csv(file_path)
 
     def read_eeg_data_from_csv(self, file_path):
         print('read file: ' + file_path)
-        self._eeg_data = np.genfromtxt(file_path, delimiter=',', names=True)
+        return np.genfromtxt(file_path, delimiter=',', names=True)
 
     def get_eeg_data(self, column_names: list):
-        return np.array(self._eeg_data[column_names].tolist())
+        return np.array(self._eeg_data[column_names].tolist()).T
 
